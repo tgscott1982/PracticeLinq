d89017df6db89d9971ca9ad643b47fafd856a403
Travis Gersch <t_gersch@hotmail.com>
2022-07-20 21:55:58 -0500

    linq exercise 2 finished

---
+++ LINQ_Exercise2/Methods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Exercise2
+{
+    internal class Methods
+    {
+    }
+}
---
+++ LINQ_Exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+
+
+string[] favGamesList = new string[]
+    { "Crisis Core: Final Fantasy 7",
+      "Final Fantasy 7",
+      "Final Fantasy 7: Remake",
+      "Final Fantasy 8",
+      "Final Fantasy 9",
+      "Final Fantasy 10/10-2",
+      "Final Fantasy 12",
+      "Final Fantasy 13/13-2/13-3",
+      "Final Fantasy 15",
+      "Persona 3: P3P",
+      "Persona 4: Golden",
+      "Persona 5: Royal",
+      "Diablo 3"};
+
+favGamesList.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine($"   {x}\n      ******   ")); //length ascending
+favGamesList.OrderByDescending(x => x.Length).ToList().ForEach(x => Console.WriteLine($"   {x}\n      ******   ")); //length descending
